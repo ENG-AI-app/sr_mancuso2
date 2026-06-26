02081c361b869e7062db14786bdc6cfe7af49943
AEng <115313550+ENG-AI-app@users.noreply.github.com>
2026-06-26 14:43:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const SUPABASE_URL = "https://eoihzwzlrjlwpdgkaifc.supabase.co";
-const SUPABASE_KEY = "sb_publishable_Tat9TUjV-ALTJoL8RoT31Q_OKuLQZUx";
+const SUPABASE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvaWh6d2pscmpsd3BkZ2thaWZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0Nzg1MTIsImV4cCI6MjA5ODA1NDUxMn0.OBupMEKHqTS8gwCulONONpet4HtCewmg8DhxRqxx5Sw";
 const COMMENTS_TABLE = "comentarios_del_juego";
 
 const gameCards = document.querySelectorAll("[data-game]");
